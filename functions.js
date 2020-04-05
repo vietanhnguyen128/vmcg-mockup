8f876723bc7b23a83ac3958c60cf85e905c9eb0b
Nguyen Viet Anh <anhnv621@wru.vn>
2020-04-05 21:49:40 +0700

    Minor changes.

--- functions.js
+++ functions.js
@@ -1,27 +1,14 @@
-var navbar = document.getElementsByClassName("navbar-container")[0];
-var menumain = document.getElementsByClassName("menu-main")[0];
-var myLogo = document.getElementById("logo");
-var navbarLink = document.getElementsByClassName("navbar-link")
+var myLogo = document.getElementById("logo")
 
 window.onscroll = function() {
   if (window.scrollY >= 50 ) {
-    navbar.classList.add("navbar-container--white");
-    menumain.classList.add("menu-main--white");
-
-    for (var item of navbarLink) {
-      item.classList.add("navbar-link--dark");
-    }
-
     myLogo.src = "../images/VMCGLogoBlack.png";
+
+
   } 
   else {
-      navbar.classList.remove("navbar-container--white");
-      menumain.classList.remove("menu-main--white");
+    myLogo.src = "../images/VMCGLogoWhiteBrown.png";
 
-      for (var item of navbarLink) {
-        item.classList.remove("navbar-link--dark");
-      }
-
-      this.myLogo.src = "../images/VMCGLogoWhiteBrown.png";
+    
   }
 };
