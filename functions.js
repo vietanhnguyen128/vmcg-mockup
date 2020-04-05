ae3e2ce21bc8eb9dfda6ed83d3967f1c7ae9b9fc
Nguyen Viet Anh <anhnv621@wru.vn>
2020-04-05 21:59:56 +0700

    Final fix.

--- functions.js
+++ functions.js
@@ -1,14 +1,18 @@
 var myLogo = document.getElementById("logo")
+var wrapper =  document.getElementsByClassName("wrapper")[0];
+var menu = document.getElementsByClassName("menu-main")[0];
 
 window.onscroll = function() {
   if (window.scrollY >= 50 ) {
     myLogo.src = "../images/VMCGLogoBlack.png";
 
-
+    wrapper.classList.add("white");
+    menu.classList.add("white");
   } 
   else {
     myLogo.src = "../images/VMCGLogoWhiteBrown.png";
 
-    
+    wrapper.classList.remove("white");
+    menu.classList.remove("white");
   }
 };
