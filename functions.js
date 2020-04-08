232519aa8b136f838164bd161286b38cbc3efa16
Nguyen Viet Anh <anhnv621@wru.vn>
2020-04-08 18:01:39 +0700

    Add animations.

--- functions.js
+++ functions.js
@@ -15,4 +15,60 @@ window.onscroll = function() {
     wrapper.classList.remove("white");
     menu.classList.remove("white");
   }
+
+  this.rangeOfBusinessAnimation();
+  this.incubatorAnimation();
+  this.contactAnimation();
 };
+
+var rangeOfBusinessAnimation = function() {
+  var $detailArticles = $(".detail-article:not(.animated)");
+
+  var $window = $(window);
+  var scrollDistance = $window.scrollTop() + $window.height();
+
+  if ($detailArticles.length !== 0) {
+    $detailArticles.each(function (index) {
+      var $this = $(this);
+  
+      var thisPos = $this.offset().top;
+  
+      if (thisPos < scrollDistance) {
+        $this.addClass("animated slideInUp");
+      }
+    })
+  }
+}
+
+var incubatorAnimation = function() {
+  var $incubatorArticles = $(".incubator-article:not(.animated)");
+
+  var $window = $(window);
+  var scrollDistance = $window.scrollTop() + $window.height();
+
+  if ($incubatorArticles.length !== 0) {
+    $incubatorArticles.each(function (index) {
+      var $this = $(this);
+  
+      var thisPos = $this.offset().top;
+  
+      if (thisPos < scrollDistance) {
+        $this.addClass("animated slideInUp");
+      }
+    })
+  }
+}
+
+var contactAnimation = function() {
+  var $banner = $(".banner-contact-us:not(.animated)");
+
+  var $window = $(window);
+  var scrollDistance = $window.scrollTop() + $window.height();
+  
+  if ($banner.length !== 0) {
+    var bannerPos = $($banner["0"]).offset().top;
+    if (bannerPos < scrollDistance) {
+      $($banner["0"]).addClass("animated fadeIn");
+    }
+  }
+}
